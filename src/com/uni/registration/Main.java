b86834f30d32f107609156afd57e7fed7d17a971
basakerdag <basakkerdag42@gmail.com>
2025-12-21 17:23:30 +0300

    Add student and instructor login access #5

--- src/com/uni/registration/Main.java
+++ src/com/uni/registration/Main.java
@@ -10,27 +10,71 @@ public class Main {
         Scanner input = new Scanner(System.in);
         CourseCatalog catalog = new CourseCatalog();
 
-        while (true) { // While artık main'in içinde
+        while (true) { 
             System.out.println("\n---Student Course Registration System---");
             System.out.println("1. Student Portal");
             System.out.println("2. Instructor Portal");
             System.out.println("3. Exit");
-            System.out.print("Please select an option: ");
+            System.out.println("Please select an option: ");
 
             int choice = input.nextInt();
             input.nextLine();
 
             if (choice == 3) {
-                System.out.println("Exiting system... Goodbye!");
+                System.out.println("Existing system... Goodbye!");
                 break; 
             }
 
             switch (choice) { 
                 case 1:
-                   //student
+                   System.out.println("---Student Portal---");
+                   System.out.println("1.Login");
+                   System.out.println("2.Register");
+                   System.out.println("3.Back");
+                   System.out.println("Please select an option: ");
+                   int studentChoice = input.nextInt();
+                   input.nextLine();
+                   if (studentChoice==3){
+                    break;
+                   }
+                   switch(studentChoice){
+                    case 1:
+                        System.out.println("---Student Login Page---");
+                    //add something
+                        break;
+                    case 2:
+                        System.out.println("---Student Register Page---");
+                        //add something
+                        break;
+                    default:
+                        System.out.println("Invalid selection.");
+                   }
+            
                     break;
                 case 2:
-                   //instructor
+                   System.out.println("---Instructor Portal---");
+                   System.out.println("1.Login");
+                   System.out.println("2.Register");
+                   System.out.println("3.Back");
+                   System.out.println("Please select an option: ");
+                   int instructorChoice=input.nextInt();
+                   input.nextLine();
+                   if(instructorChoice==3){
+                    break;
+                   }
+                   switch(instructorChoice){
+                    case 1:
+                        System.out.println("---Instructor Login Page");
+                        //add something
+                        break;
+                    case 2:
+                        System.out.println("---Instructor Register Page---");
+                        //add something
+                        break;
+                    default:
+                        System.out.println("Invalid selection.");
+                   }
+
                     break;
                 default:
                     System.out.println("Invalid selection.");
